57046d3606f6091b23c0ecc5e91599d1c0ed6198
Marcel Susky <marcel.susky@tu-dresden.de>
2023-01-24 16:47:36 +0100

    Switches back from MII KDS Person v1.0.16 to v1.0.14 dependency

--- _updateSimplifier.sh
+++ _updateSimplifier.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-find ./output -name "*.json.zip" -type f -exec unzip -ud ./.simplifier/resources {} "*.json" \;
+find ./output -name "*.json.zip" -type f -exec unzip -uod ./.simplifier/resources {} "*.json" \;
